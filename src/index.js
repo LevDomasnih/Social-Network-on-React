6660942f1b7aaab38682d3d45dc56bc5c977c153
LevDomasnih <55681260+LevDomasnih@users.noreply.github.com>
2020-12-18 17:58:50 +0500

    add context

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from "./redux/reduxStore";
 import {BrowserRouter} from "react-router-dom";
+import StoreContext from "./StoreContext";
 
 let rerenderEntireTree = (state) => {
     ReactDOM.render(
         <BrowserRouter>
-            <App state={state} dispatch={store.dispatch.bind(store)} store={store} />
+            <StoreContext.Provider value={store}>
+                <App />
+            </StoreContext.Provider>
         </BrowserRouter>,
         document.getElementById('root')
     );
