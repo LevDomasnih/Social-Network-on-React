936affbf60479abc2f3605ef4c296ac17dfe1f9f
LevDomasnih <lev.domasnix2013@gmail.com>
2021-01-10 04:40:05 +0500

    v0.83 use => reselect (createSelector); refactor => reselect for UsersContainer.jsx; add to npm => reselect; create => nothing; edit => usersSelectors.js

--- src/redux/usersSelectors.js
+++ src/redux/usersSelectors.js
@@ -1,18 +1,47 @@
-export const getUsers = (state) => {
+import {createSelector} from "reselect";
+
+const getUsersSelector = (state) => {
     return state.usersPage.users
 }
-export const getPageSize = (state) => {
+const getPageSizeSelector = (state) => {
     return state.usersPage.pageSize
 }
-export const getTotalUsersCount = (state) => {
+const getTotalUsersCountSelector = (state) => {
     return state.usersPage.totalUsersCount
 }
-export const getCurrentPage = (state) => {
+const getCurrentPageSelector = (state) => {
     return state.usersPage.currentPage
 }
-export const getIsFetching = (state) => {
+const getIsFetchingSelector = (state) => {
     return state.usersPage.isFetching
 }
-export const getFollowingInProgress = (state) => {
+const getFollowingInProgressSelector = (state) => {
     return state.usersPage.followingInProgress
 }
+
+
+export const getUsers = createSelector(getUsersSelector,
+    (users) => {
+    return users;
+})
+export const getPageSize = createSelector(getPageSizeSelector,
+    (pageSize) => {
+    return pageSize;
+})
+export const getTotalUsersCount = createSelector(getTotalUsersCountSelector,
+    (totalUsersCount) => {
+    return totalUsersCount;
+})
+export const getCurrentPage = createSelector(getCurrentPageSelector,
+    (currentPage) => {
+    return currentPage;
+})
+export const getIsFetching = createSelector(getIsFetchingSelector,
+    (isFetching) => {
+    return isFetching;
+})
+export const getFollowingInProgress = createSelector(getFollowingInProgressSelector,
+    (followingInProgress) => {
+    return followingInProgress;
+})
+
