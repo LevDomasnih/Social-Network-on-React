646eafa64bf8ff69d0b2c4b3b4f5d27271a01895
LevDomasnih <lev.domasnix2013@gmail.com>
2021-01-10 23:43:58 +0500

    v0.89 use => Tests, Jest, tdd; refactor => Testing of Reducer; add to npm => nothing; create => profileReducer.test.js; delete => ProfileStatus.jsx, App.test.js; edit => ProfileInfo.jsx, profileReducer.js

--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -3,6 +3,8 @@ import {ProfileAPI} from "../API/api";
 const ADD_POST = 'ADD-POST';
 const SET_USER_PROFILE = 'SET_USER_PROFILE';
 const SET_STATUS = 'SET_STATUS';
+const DELETE_POST = 'DELETE_POST';
+
 
 let initialState = {
     posts: [
@@ -27,6 +29,8 @@ const profileReducer = (state = initialState, action) => {
             return {...state, profile: action.profile};
         case SET_STATUS:
             return {...state, status: action.status};
+        case DELETE_POST:
+            return {...state, posts: state.posts.filter(p => p.id != action.postId)}
         default:
             return state;
     }
@@ -36,6 +40,8 @@ export default profileReducer;
 export const addPost = (newPost) => ({type: ADD_POST, newPost});
 export const setUsersProfile = (profile) => ({type: SET_USER_PROFILE, profile});
 export const setStatus = (status) => ({type: SET_STATUS, status});
+export const deletePost = (postId) => ({type: DELETE_POST, postId})
+
 
 export const getUserProfile = (userId) => (dispatch) => {
     ProfileAPI.getUserProfile(userId)
---
+++ src/redux/profileReducer.test.js
@@ -0,0 +1,57 @@
+import profileReducer, {addPost, deletePost} from "./profileReducer.js";
+import ReactDOM from "react-dom";
+import App from "../App";
+import React from "react";
+
+let state = {
+    posts: [
+        {id: 1, message: 'Hi, how are you?', likesCount: 105},
+        {id: 2, message: "It's my first post", likesCount: 10},
+        {id: 3, message: "Lol", likesCount: 5000},
+        {id: 4, message: "Kek", likesCount: 9400},
+    ]
+};
+
+test('length of posts should be incremented', () => {
+    // 1. test data
+    let action = addPost("Hello!!!!");
+
+    // 2. action
+    let newState = profileReducer(state, action);
+
+    // 3. expectation
+    expect(newState.posts.length).toBe(5);
+});
+
+test('message of new post should be correct', () => {
+    // 1. test data
+    let action = addPost("New Test");
+
+    // 2. action
+    let newState = profileReducer(state, action);
+
+    // 3. expectation
+    expect(newState.posts[0].message).toBe("New Test");
+});
+
+test('after deleting length of messages should be decrement', () => {
+    // 1. test data
+    let action = deletePost(1);
+
+    // 2. action
+    let newState = profileReducer(state, action);
+
+    // 3. expectation
+    expect(newState.posts.length).toBe(3);
+});
+
+test(`after deleting length shouldn't be decrement if id is incorrect`, () => {
+    // 1. test data
+    let action = deletePost(1000);
+
+    // 2. action
+    let newState = profileReducer(state, action);
+
+    // 3. expectation
+    expect(newState.posts.length).toBe(4);
+});
