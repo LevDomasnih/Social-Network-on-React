329935989b036b98a2076a42324a831da97d3131
LevDomasnih <lev.domasnix2013@gmail.com>
2021-01-12 03:38:20 +0500

    v0.92 use => Testing Components; description => return App.test.js, Test Components; add to npm => react-test-renderer; create => App.test.js, ProfileStatus.test.jsx; delete => nothing; edit => App.js, index.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import React from 'react';
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
-import {Route, withRouter} from "react-router-dom"
+import {BrowserRouter, Route, withRouter} from "react-router-dom"
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import NavbarContainer from "./components/Navbar/NavbarContainer";
 import Login from "./components/Login/Login";
-import {connect} from "react-redux";
+import {connect, Provider} from "react-redux";
 import {compose} from "redux";
 import {initializeApp} from "./redux/appReducer";
 import Preloader from "./components/common/Preloader/Preloader";
+import store from "./redux/reduxStore";
 
 
 class App extends React.Component {
@@ -23,23 +24,23 @@ class App extends React.Component {
 
     render() {
         if (!this.props.initialized) {
-            return <Preloader />
+            return <Preloader/>
         }
 
         return (
             <div className='app-wrapper'>
-                <HeaderContainer />
-                <NavbarContainer />
+                <HeaderContainer/>
+                <NavbarContainer/>
                 <div className='app-wrapper-content'>
                     <Route path='/profile/:userId?'
-                           render={() => <ProfileContainer />} />
+                           render={() => <ProfileContainer/>}/>
                     <Route path='/dialogs'
-                           render={() => <DialogsContainer />} />
-                    <Route path='/users' render={() => <UsersContainer />} />
-                    <Route path='/news' render={() => <News />} />
-                    <Route path='/music' render={() => <Music />} />
-                    <Route path='/settings' render={() => <Settings />} />
-                    <Route path='/login' render={() => <Login />} />
+                           render={() => <DialogsContainer/>}/>
+                    <Route path='/users' render={() => <UsersContainer/>}/>
+                    <Route path='/news' render={() => <News/>}/>
+                    <Route path='/music' render={() => <Music/>}/>
+                    <Route path='/settings' render={() => <Settings/>}/>
+                    <Route path='/login' render={() => <Login/>}/>
                 </div>
             </div>
         );
@@ -50,7 +51,16 @@ const mapStateToProps = (state) => ({
     initialized: state.app.initialized,
 })
 
-export default compose(
+let AppContainer = compose(
     withRouter,
-    connect(mapStateToProps, { initializeApp })
-)(App);
+    connect(mapStateToProps, {initializeApp}))(App);
+
+const SocialNetworkApp = (props) => {
+    return <BrowserRouter>
+        <Provider store={store}>
+            <AppContainer/>
+        </Provider>
+    </BrowserRouter>
+}
+
+export default SocialNetworkApp;
---
+++ src/components/Profile/ProfileInfo/ProfileStatus.test.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { create } from "react-test-renderer";
+import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
+
+describe("ProfileStatus component", () => {
+    test("after creation <span> should be displayed", () => {
+        const component = create(<ProfileStatusWithHooks status="Hello!!!" />);
+        const root = component.root;
+        let span = root.findByType("span");
+        expect(span).not.toBeNull();
+    });
+    test("after creation <input> shouldn't be displayed", () => {
+        const component = create(<ProfileStatusWithHooks status="Hello!!!" />);
+        const root = component.root;
+        expect(() => {
+            let input = root.findByType("Hello!!!");
+        }).toThrow();
+    });
+
+    test("after creation <span> should contains correct status", () => {
+        const component = create(<ProfileStatusWithHooks status="Hello!!!" />);
+        const root = component.root;
+        let span = root.findByType("span");
+        expect(span.children[0]).toBe("Hello!!!");
+    });
+
+    test("input should be displayed in editMode instead of span", () => {
+        const component = create(<ProfileStatusWithHooks status="Hello!!!" />);
+        const root = component.root;
+        let span = root.findByType("span");
+        span.props.onDoubleClick();
+        let input = root.findByType("input");
+        expect(input.props.value).toBe("Hello!!!");
+    });
+});
