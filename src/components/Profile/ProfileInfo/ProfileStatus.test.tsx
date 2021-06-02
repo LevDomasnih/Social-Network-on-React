39b37c217db93366c3d0edd9760b5fe61c2d8673
lev <lev.domasnix2013@gmail.com>
2021-06-03 02:10:21 +0500

    v1.17.7 description => fix tests; use => Jest; add to npm => nothing; create => nothing; delete => nothing; edit => ProfileStatus.test.tsx

--- src/components/Profile/ProfileInfo/ProfileStatus.test.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.test.tsx
@@ -4,13 +4,13 @@ import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 
 describe("ProfileStatus component", () => {
     test("after creation <span> should be displayed", () => {
-        const component = create(<ProfileStatusWithHooks status="Hello!!!" />);
+        const component = create(<ProfileStatusWithHooks status="Hello!!!" isOwner={true} />);
         const root = component.root;
         let span = root.findByType("span");
         expect(span).not.toBeNull();
     });
     test("after creation <input> shouldn't be displayed", () => {
-        const component = create(<ProfileStatusWithHooks status="Hello!!!" />);
+        const component = create(<ProfileStatusWithHooks status="Hello!!!" isOwner={true} />);
         const root = component.root;
         expect(() => {
             root.findByType("input");
@@ -18,14 +18,14 @@ describe("ProfileStatus component", () => {
     });
 
     test("after creation <span> should contains correct status", () => {
-        let component = create(<ProfileStatusWithHooks status="Hello!!!" />);
+        let component = create(<ProfileStatusWithHooks status="Hello!!!" isOwner={true} />);
         const root = component.root;
         let span = root.findByType("span");
         expect(span.props.children).toBe("Hello!!!");
     });
 
     test("input should be displayed in editMode instead of span", () => {
-        const component = create(<ProfileStatusWithHooks status="Hello!!!" />);
+        const component = create(<ProfileStatusWithHooks status="Hello!!!" isOwner={true} />);
         const root = component.root;
         let span = root.findByType("span");
         act(() => {
@@ -33,5 +33,13 @@ describe("ProfileStatus component", () => {
         })
         let input = root.findByType("input");
         expect(input.props.value).toBe("Hello!!!");
+    });
+
+    test("input not should be displayed if isOwner - false", () => {
+        const component = create(<ProfileStatusWithHooks status="Hello!!!" isOwner={false} />);
+        const root = component.root;
+        expect(() => {
+            root.findByType("input");
+        }).toThrow();
     });
 });
